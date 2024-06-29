1cbbaf173f3584fd671d73d0bd4e99c0826f942c
Angelldca <angeluisdelcollado@gmail.com>
2024-06-28 21:17:09 -0400

    deploy render superuser

--- build.sh
+++ build.sh
@@ -14,5 +14,3 @@ python manage.py migrate
 
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
 
-# Inicia la aplicación
-gunicorn --bind 0.0.0.0:8000 myproject.wsgi:application
