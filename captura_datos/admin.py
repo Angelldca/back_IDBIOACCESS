bccc4328e0c14d41c65e5682449e40f35f3371bc
Angelldca <angeluisdelcollado@gmail.com>
2024-07-02 20:00:17 -0400

    admin causa anulacion import

--- captura_datos/admin.py
+++ captura_datos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Dusuario, Dciudadano, Dciudadanobash, Dsolapin,Ntiposolapin, Dimagenfacial, Nestado, Dregistropago
+from .models import Dusuario, Dciudadano,Ncausaanulacion, Dciudadanobash, Dsolapin,Ntiposolapin, Dimagenfacial, Nestado, Dregistropago
 # Register your models here.
 admin.site.register(Dusuario)
 admin.site.register(Dciudadano)
